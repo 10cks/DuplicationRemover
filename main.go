33fba0b8ab967214883562f4167c296bc7f6e103
10cks <47177550+10cks@users.noreply.github.com>
2024-01-19 16:05:04 +0800

    主函数

---
+++ main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"os"
+)
+
+func main() {
+
+	// 定义一个新的字符串参数，name为"f", 默认值为"domain.txt", 使用方法说明为"指定文件"
+	filePath := flag.String("f", "domain.txt", "指定文件")
+
+	// 使用flag参数
+	err := RemoveDuplicates(*filePath)
+	if err != nil {
+		panic(err)
+	} else {
+		fmt.Println("Run successfully.")
+	}
+}
+
+func RemoveDuplicates(filePath string) error {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return err
+	}
+
+	seen := map[string]bool{}
+	scanner := bufio.NewScanner(file)
+	outfile, err := os.Create("outfile.txt")
+	if err != nil {
+		return err
+	}
+
+	writer := bufio.NewWriter(outfile)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		if !seen[line] {
+			seen[line] = true
+			_, err := writer.WriteString(line + "\n")
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		return err
+	}
+
+	err = writer.Flush()
+	if err != nil {
+		return err
+	}
+
+	err = outfile.Close()
+	if err != nil {
+		return err
+	}
+
+	// 显式地关闭旧文件
+	err = file.Close()
+	if err != nil {
+		return err
+	}
+
+	err = os.Remove(filePath)
+	if err != nil {
+		return err
+	}
+
+	err = os.Rename("outfile.txt", filePath)
+
+	return err
+}
